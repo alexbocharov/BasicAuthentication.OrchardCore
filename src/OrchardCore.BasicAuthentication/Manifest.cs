29c16f6294bc120d37af9f774452c165c171df11
Alexander Bocharov <alex.bocharov@outlook.com>
2019-04-04 16:39:33 +0300

    Update manifest

--- src/OrchardCore.BasicAuthentication/Manifest.cs
+++ src/OrchardCore.BasicAuthentication/Manifest.cs
@@ -2,9 +2,9 @@ using OrchardCore.Modules.Manifest;
 
 [assembly: Module(
     Name = "Basic Authentication",
-    Author = "The Orchard Team",
-    Website = "https://orchardproject.net",
-    Version = "0.0.1",
-    Description = "The Basic Authentication module enables authentication of external apps through Basic Authentication",
+    Author = "Alexander Bocharov",
+    Website = "https://github.com/alexbocharov/OrchardCore.BasicAuthentication",
+    Version = "1.0.0",
+    Description = "The Basic Authentication module enables authentication of external apps through Basic Authentication.",
     Category = "Basic Authentication"
 )]
